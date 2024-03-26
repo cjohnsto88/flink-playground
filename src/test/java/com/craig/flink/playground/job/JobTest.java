bdc840dc5f3728fc1ab83fc947bba0b19f38976e
Craig Johnston <cjohnsto88@gmail.com>
2024-03-26 17:26:20 +0000

    Got Flink Mini Cluster working in JUnit 5 test

--- src/test/java/com/craig/flink/playground/job/JobTest.java
+++ src/test/java/com/craig/flink/playground/job/JobTest.java
@@ -1,7 +1,14 @@
 package com.craig.flink.playground.job;
 
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.runtime.testutils.MiniClusterResourceConfiguration;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+import org.apache.flink.test.junit5.MiniClusterExtension;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.KafkaContainer;
@@ -10,29 +17,71 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Testcontainers
+@ExtendWith(MiniClusterExtension.class)
 class JobTest {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Job.class);
+
+
+    @RegisterExtension
+    static MiniClusterExtension miniClusterExtension = new MiniClusterExtension(
+            new MiniClusterResourceConfiguration.Builder()
+                    .setNumberTaskManagers(1)
+                    .setNumberSlotsPerTaskManager(2)
+                    .build()
+    );
 
     @Container
     KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:6.2.1"))
             .withLogConsumer(new Slf4jLogConsumer(LOGGER));
 
-
     @BeforeEach
     void setUp() {
         LOGGER.info("Hello");
-
-        System.out.println("setup");
     }
 
     @Test
-    void containerHasBootstrapServersValue() {
+    void containerHasBootstrapServersValue() throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // configure your test environment
+        env.setParallelism(2);
+
+        // values are collected in a static variable
+        CollectSink.values.clear();
+
+        // create a stream of custom elements and apply transformations
+        env.fromElements(1L, 21L, 22L)
+           .map((MapFunction<Long, Long>) value -> value + 1)
+           .addSink(new CollectSink());
+
+        // execute
+        env.execute();
+
+        // verify your results
+        assertThat(CollectSink.values).containsExactly(2L, 22L, 23L);
+
         String bootstrapServers = kafka.getBootstrapServers();
 
         assertThat(bootstrapServers).isNotEmpty();
+    }
+
+
+    private static class CollectSink implements SinkFunction<Long> {
+
+        // must be static
+        public static final List<Long> values = Collections.synchronizedList(new ArrayList<>());
+
+        @Override
+        public void invoke(Long value, SinkFunction.Context context) throws Exception {
+            values.add(value);
+        }
     }
 }
