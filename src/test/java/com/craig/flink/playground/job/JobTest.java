65dbe5c40b3e56caf892f2dd1ccd494ce91a90d2
Craig Johnston <cjohnsto88@gmail.com>
2024-03-27 15:32:00 +0000

    Additional logging to help diagnose failure in GitHub Actions

--- src/test/java/com/craig/flink/playground/job/JobTest.java
+++ src/test/java/com/craig/flink/playground/job/JobTest.java
@@ -1,6 +1,9 @@
 package com.craig.flink.playground.job;
 
+import org.apache.flink.client.program.MiniClusterClient;
+import org.apache.flink.runtime.client.JobStatusMessage;
 import org.apache.flink.runtime.testutils.MiniClusterResourceConfiguration;
+import org.apache.flink.test.junit5.InjectClusterClient;
 import org.apache.flink.test.junit5.MiniClusterExtension;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.AdminClientConfig;
@@ -25,15 +28,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.testcontainers.containers.KafkaContainer;
-import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -43,11 +47,10 @@ import static org.awaitility.Awaitility.await;
 @Testcontainers
 class JobTest {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(Job.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(JobTest.class);
 
     public static final String INPUT_TOPIC = "first-flink-data";
     public static final String OUTPUT_TOPIC = "first-flink-uppercase";
-    public static final Slf4jLogConsumer LOG_CONSUMER = new Slf4jLogConsumer(LOGGER);
 
     @RegisterExtension
     static MiniClusterExtension miniClusterExtension = new MiniClusterExtension(
@@ -58,21 +61,18 @@ class JobTest {
     );
 
     @Container
-    static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.4.4"))
-            .withLogConsumer(LOG_CONSUMER);
+    static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.4.4"));
 
     private ExecutorService jobSubmitter;
 
     @BeforeAll
     static void beforeAll() {
-        kafka.followOutput(LOG_CONSUMER);
-
         Map<String, Object> config = Map.of(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers());
 
         try (AdminClient adminClient = AdminClient.create(config)) {
             adminClient.createTopics(List.of(
-                    new NewTopic(INPUT_TOPIC, 4, (short) 1),
-                    new NewTopic(OUTPUT_TOPIC, 4, (short) 1)
+                    new NewTopic(INPUT_TOPIC, 2, (short) 1),
+                    new NewTopic(OUTPUT_TOPIC, 2, (short) 1)
             ));
         }
     }
@@ -88,7 +88,7 @@ class JobTest {
     }
 
     @Test
-    void jobConvertsStringToUppercase() {
+    void jobConvertsStringToUppercase(@InjectClusterClient MiniClusterClient miniClusterClient) {
         Map<String, Object> producerProperties = KafkaTestUtils.producerProps(kafka.getBootstrapServers());
 
         try (Producer<String, String> producer = new KafkaProducer<>(producerProperties, new StringSerializer(), new StringSerializer())) {
@@ -126,7 +126,75 @@ class JobTest {
             await().atMost(Duration.ofSeconds(20L))
                    .untilAsserted(() -> {
                        try {
-                           ConsumerRecords<String, String> records = KafkaTestUtils.getRecords(consumer);
+                           ConsumerRecords<String, String> records = KafkaTestUtils.getRecords(consumer, Duration.ofSeconds(2L));
+                           records.forEach(receivedRecords::add);
+
+                           LOGGER.info("receivedRecords: {}", receivedRecords);
+
+                           assertThat(receivedRecords).allSatisfy(record -> {
+                               LOGGER.info("Record: {}", record);
+
+                               JSONAssert.assertEquals(expectedJson, record.value(), JSONCompareMode.STRICT);
+                           });
+                       } catch (IllegalStateException e) {
+                           // ignore
+                       } catch (Exception e) {
+                           LOGGER.error("Failure when asserting", e);
+
+                           throw e;
+                       }
+
+                       try {
+                           Collection<JobStatusMessage> jobStatusMessages = miniClusterClient.listJobs().get();
+
+                           jobStatusMessages.forEach(message -> LOGGER.info("Name: {}, State: {}", message.getJobName(), message.getJobState()));
+                       } catch (InterruptedException | ExecutionException e) {
+                           throw e;
+                       }
+                   });
+        }
+    }
+
+    @Test
+    void jobConvertsStringToUppercase2() {
+        Map<String, Object> producerProperties = KafkaTestUtils.producerProps(kafka.getBootstrapServers());
+
+        try (Producer<String, String> producer = new KafkaProducer<>(producerProperties, new StringSerializer(), new StringSerializer())) {
+            // language=json
+            String json = """
+                    {
+                      "firstName": "Craig",
+                      "lastName": "Johnston"
+                    }
+                    """;
+
+            producer.send(new ProducerRecord<>(INPUT_TOPIC, "{\"key\": \"ONE\"}", json));
+        }
+
+        jobSubmitter.submit(() -> {
+            Job job = new Job();
+
+            return job.execute(kafka.getBootstrapServers());
+        });
+
+        Map<String, Object> consumerProperties = KafkaTestUtils.consumerProps(kafka.getBootstrapServers(), "test-group", "true");
+
+        // language=json
+        String expectedJson = """
+                {
+                  "firstName": "CRAIG",
+                  "lastName": "JOHNSTON"
+                }
+                """;
+
+        List<ConsumerRecord<String, String>> receivedRecords = new ArrayList<>();
+        try (Consumer<String, String> consumer = new KafkaConsumer<>(consumerProperties, new StringDeserializer(), new StringDeserializer())) {
+            consumer.subscribe(List.of(OUTPUT_TOPIC));
+
+            await().atMost(Duration.ofSeconds(20L))
+                   .untilAsserted(() -> {
+                       try {
+                           ConsumerRecords<String, String> records = KafkaTestUtils.getRecords(consumer, Duration.ofSeconds(2L));
                            records.forEach(receivedRecords::add);
 
                            LOGGER.info("receivedRecords: {}", receivedRecords);
